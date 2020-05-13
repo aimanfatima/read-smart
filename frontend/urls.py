865ea90d92d837c1d79c769a4caef1de16ea0a24
Aiman Fatima <aimanfatimadl@gmail.com>
2020-05-13 22:29:17 +0530

    Add pdf view

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     homepage,
-    doc_view
+    doc_view,
+    pdf_view
 )
 
 urlpatterns = [
     path("", homepage),
-    path("show_doc/", doc_view)
+    path("show_doc/", doc_view), 
+    path("read_pdf/", pdf_view)
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from docx import Document
-from .utils import doc_to_pdf_converter
+from .utils import doc_to_pdf_converter, pdfparser
 import requests
 import json
 
@@ -56,3 +57,39 @@ def doc_view(request):
             # 'questions': questions
         }
         return render(request, 'doc_view.html', context=context, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'POST'])
+@csrf_exempt
+def pdf_view(request):
+        try:
+            file = request.FILES["file"]
+            print(file.content_type)
+        except KeyError:
+            return Response(
+                {"message": "File not uploaded"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        if file.content_type != "application/pdf":
+            return Response(
+                {"message": "File type not supported"},
+                status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            )
+        ## DO SOMETHING WITH FILE ##
+        filename = default_storage.save("static/uploaded_file.pdf",file)
+        uploaded_file_url = default_storage.url(filename)
+
+        # pdf = fitz.open(uploaded_file_url)
+        # print ("number of pages:", pdf.pageCount)
+        text_page_wise = pdfparser(uploaded_file_url)
+        print(type(text_page_wise))
+        print(type(text_page_wise[0]))
+        print(filename)
+        # questions_page_wise = get_questions_page_wise(text_page_wise)
+        ##############################
+        context = {
+            'filename':filename.split("static/",1)[1],
+            'no_of_pages': len(text_page_wise), 
+            'text_page_wise': text_page_wise,
+            'text_page_wise_json': json.dumps(text_page_wise)
+            # 'questions_page_wise': questions_page_wise
+        }
+        return render(request, 'pdf_view.html', context=context, status=status.HTTP_201_CREATED)
